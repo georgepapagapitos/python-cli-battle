ec06f10b41ba428cb03eca875324337c6c3d5137
George Papagapitos <george.g.papagapitos@gmail.com>
2021-05-02 20:08:32 -0500

    stat bars not aligning

--- classes/game.py
+++ classes/game.py
@@ -76,12 +76,16 @@ class Person:
         i = 1
         print("\n" + bcolors.OKGREEN + bcolors.BOLD + "    ITEMS" + bcolors.ENDC)
         for item in self.items:
-            print("        " + str(i) + ".", item["item"].name, ":", item["item"].description, "(x" + str(item["quantity"]) + ")")
+            print("        " + str(i) + ".", item["item"].name, ":", item["item"].description,
+                  "(x" + str(item["quantity"]) + ")")
             i += 1
 
     def get_stats(self):
         hp_bar = ""
         bar_ticks = (self.hp / self.max_hp) * 100 / 4
+
+        mp_bar = ""
+        mp_ticks = (self.mp / self.max_mp) * 100 / 10
 
         while bar_ticks >= 0:
             hp_bar += "█"
@@ -90,9 +94,41 @@ class Person:
         while len(hp_bar) < 25:
             hp_bar += " "
 
-        print("                    _________________________           __________ ")
+        while mp_ticks >= 0:
+            mp_bar += "█"
+            mp_ticks -= 1
+
+        while len(mp_bar) < 10:
+            mp_bar += " "
+
+        hp_string = str(self.hp) + "/" + str(self.max_hp)
+        current_hp = ""
+
+        mp_string = str(self.mp) + "/" + str(self.max_mp)
+        current_mp = ""
+
+        if len(hp_string) < 9:
+            decreased = 9 - len(hp_string)
+
+            while decreased > 0:
+                current_hp += " "
+                decreased -= 1
+
+            current_hp += hp_string
+        else:
+            current_hp = hp_string
+
+        if len(mp_string) < 7:
+            decreased = 7 - len(mp_string)
+            while decreased > 0:
+                current_mp += " "
+                decreased -= 1
+
+            current_mp += mp_string
+        else:
+            current_mp = mp_string
+
+        print('                     __________________________            ___________ ')
         print(bcolors.BOLD + self.name + ":" + "     " +
-              str(self.hp) + "/" + str(self.max_hp) + "|" + bcolors.OKGREEN + hp_bar + bcolors.ENDC +
-              bcolors.BOLD + "|    " +
-              str(self.mp) + "/" + str(self.max_mp) + "|" + bcolors.OKBLUE + "██████████" + bcolors.ENDC + bcolors.BOLD + "|" +
-              bcolors.ENDC)
+              current_hp + " |" + bcolors.OKGREEN + hp_bar + bcolors.ENDC + bcolors.BOLD + "|    " +
+              current_mp + " |" + bcolors.OKBLUE + mp_bar + bcolors.ENDC + bcolors.BOLD + "|" + bcolors.ENDC)
